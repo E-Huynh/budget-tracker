843b279b9f22f3f305bd1c6586683bef48acce6e
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-03-30 21:44:12 -0600

    Add PORT change

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://<user1>:<password1>@ds145694.mlab.com:45694/heroku_9fq55nmx"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 const options = {
   useNewUrlParser: true,
   useFindAndModify: false,
